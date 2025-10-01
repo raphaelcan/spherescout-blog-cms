e0056f75e2a371aff10614dec3209d142f388abf
raphaelcanyasse <raphael.canyasse@gmail.com>
2025-10-01 11:35:04 +0200

    more + debug

--- src/app/(payload)/admin/[[...segments]]/page.tsx
+++ src/app/(payload)/admin/[[...segments]]/page.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next'
 import config from '@payload-config'
 import { RootPage, generatePageMetadata } from '@payloadcms/next/views'
 import { importMap } from '../importMap'
+export const runtime = 'nodejs';
 
 type Args = {
   params: Promise<{
